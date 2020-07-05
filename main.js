fc6944bf3613d9c035b04fb2a4e9c266ce9dd674
antoniorea <antonioreacifuentes@yahoo.es>
2020-07-05 23:25:26 +0200

    Un mejor archivo main.js

--- main.js
+++ main.js
@@ -1,39 +1,35 @@
 "use strict";
 
-const homeController = require("./controllers/homeController");
-const errorController = require("./controllers/errorController");
-const express = require("express");
-const app = express();
-const layouts = require("express-ejs-layouts");
+const express = require("express"),
+  app = express(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts");
+
 app.set("view engine", "ejs");
-app.use(layouts);
-// Adding body parsing
+app.set("port", process.env.PORT || 3000);
 app.use(
-    express.urlencoded({
-        extended: false
-    })
+  express.urlencoded({
+    extended: false
+  })
 );
 app.use(express.json());
-
-//Port 3000
-app.set("port", process.env.PORT || 3000);
-// Serving static views
+app.use(layouts);
 app.use(express.static("public"));
-// Routes
+
 app.get("/", (req, res) => {
-    res.send("Welcome to Confetti Cuisine!");    
+  res.render("index");
 });
+
 app.get("/courses", homeController.showCourses);
 app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
+
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
 
-//Server listen in port 3000
 app.listen(app.get("port"), () => {
-    console.log(
-        `Server running at http://localhost:${app.get("port")}`
-    );
+  console.log(`Server running at http://localhost:${app.get("port")}`);
 });
 
     
